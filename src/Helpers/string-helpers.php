cc54b69460481eaf57a34e79711c6f7fa8ef2679
Daniel Leppänen <daniel.leppanen@gmail.com>
2018-12-05 01:30:55 +0100

    Added camel_case and snake_case to string-helpers

--- src/Helpers/string-helpers.php
+++ src/Helpers/string-helpers.php
@@ -1,6 +1,31 @@
 <?php
 
 /**
+ *  @param String $string   The string to camelCase
+ *  @author Halpdesk
+ */
+if (!function_exists('camel_case')) {
+    function camel_case(string $string)
+    {
+        $chars = ['-', '_', ' '];
+        return lcfirst(str_replace($chars, '', ucwords($string, implode($chars))));
+    }
+}
+
+/**
+ *  @param String $string   The string to camelCase
+ *  @author Halpdesk
+ */
+if (!function_exists('snake_case')) {
+    function snake_case(string $string)
+    {
+        $chars = ['-', ' '];
+        // u = treated as UTF8
+        return strtolower(str_replace($chars, '_', preg_replace('/(.)(?=[A-Z])/u', '$1_', $string)));
+    }
+}
+
+/**
  *  replace_foregin_chars - will return a string with foreign characters like å, ä, ö replaced with a, a, o
  *
  *  There are probably many ways to do this for all characters, but the blow answer does not work for all encodings:
@@ -12,20 +37,17 @@
 if (!function_exists('replace_foreign_chars')) {
     function replace_foreign_chars(string $string)
     {
-
-        $foreign = ['ç', 'é', 'í', 'û', 'ü', 'å', 'ä', 'æ', 'ö', 'õ', 'ø'];
-        $safe    = ['c', 'e', 'i', 'u', 'u', 'a', 'a', 'ae', 'o', 'o', 'o'];
-
-        $foreignUpper = ['É', 'Í', 'Û', 'Ü', 'Å', 'Ä', 'Æ', 'Õ', 'Ö', 'Ø'];
-        $safeUpper    = ['E', 'I', 'U', 'U', 'A', 'A', 'AE', 'O', 'O', 'O'];
-
-        $safeString = str_replace(
-            array_merge($foreign, $foreignUpper),
-            array_merge($safe, $safeUpper),
-            $string
-        );
-
-        return $safeString;
+        $foreign = [
+            'ç',
+            'é', 'í', 'û', 'ü', 'å', 'ä', 'æ', 'ö', 'õ', 'ø',
+            'É', 'Í', 'Û', 'Ü', 'Å', 'Ä', 'Æ', 'Õ', 'Ö', 'Ø'
+        ];
+        $safe = [
+            'c',
+            'e', 'i', 'u', 'u', 'a', 'a', 'ae', 'o', 'o', 'o',
+            'E', 'I', 'U', 'U', 'A', 'A', 'AE', 'O', 'O', 'O'
+        ];
+        return str_replace($foreign, $safe, $string);
     }
 }
 
@@ -34,10 +56,10 @@ if (!function_exists('replace_foreign_chars')) {
  * @param string $string    The string to manipulate
  * @return string           The stripped string
  */
-if (!function_exists('convert_to_alphanumeric')) {
-    function convert_to_alphanumeric($string)
+if (!function_exists('strip_to_alphanumeric')) {
+    function strip_to_alphanumeric($string)
     {
-        $string = preg_replace('/[^a-z0-9\s-_]/i', '', $string);
+        $string = preg_replace('/[^a-zA-Z0-9\s-_]/i', '', $string);
         return $string;
     }
 }
