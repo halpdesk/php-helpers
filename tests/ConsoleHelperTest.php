3f6d2ba28c1c7b38fe7cbbd8f61f852dc463e5f8
Daniel Leppänen <daniel.leppanen@gmail.com>
2018-12-05 02:22:45 +0100

    Updated ConsoleHelperTest with tests for all console functions

--- tests/ConsoleHelperTest.php
+++ tests/ConsoleHelperTest.php
@@ -15,7 +15,18 @@ class ConsoleHelperTest extends TestCase
      */
     public function test_cc()
     {
-        $this->markTestSkipped();
+        $this->assertEquals(
+            "\033[0;36m\033[42mHello world\033[0m",
+            cc('Hello world', 'cyan', 'green')
+        );
+        $this->assertEquals(
+            "\033[43mHello world\033[0m",
+            cc('Hello world', null, 'yellow')
+        );
+        $this->assertEquals(
+            "\033[0;37mHello world\033[0m",
+            cc('Hello world', 'light_gray', null)
+        );
     }
 
     /**
@@ -24,16 +35,38 @@ class ConsoleHelperTest extends TestCase
      */
     public function test_eject()
     {
-        $this->markTestSkipped();
+        // Since eject prints the string directly, it must be captured by the output buffer
+        ob_start();
+        eject(["foo" => "baz", "bar" => "buz"], 2, 'blue', 'brown');
+        $ejectString = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals("  {
+    \"\033[0;34mfoo\033[0m\": \"\033[0;33mbaz\033[0m\",
+    \"\033[0;34mbar\033[0m\": \"\033[0;33mbuz\033[0m\"
+  }",
+            $ejectString
+        );
     }
 
     /**
-     * @covers jdd()
+     * @covers edd()
      * @group console
      */
-    public function test_jdd()
+    public function test_edd()
     {
-        $this->markTestSkipped();
+        // Since edd prints and dies directly, it must be captured by the output buffer
+        ob_start();
+        eject(["foo" => "baz", "bar" => "buz"], 2, 'blue', 'brown');
+        $ejectString = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals("  {
+    \"\033[0;34mfoo\033[0m\": \"\033[0;33mbaz\033[0m\",
+    \"\033[0;34mbar\033[0m\": \"\033[0;33mbuz\033[0m\"
+  }",
+            $ejectString
+        );
     }
 
     /**
@@ -42,7 +75,49 @@ class ConsoleHelperTest extends TestCase
      */
     public function test_output()
     {
-        $this->markTestSkipped();
+        // Since output prints the string directly, it must be captured by the output buffer
+        ob_start();
+        output('Hello world', 'info');
+        $outputInfoString = ob_get_contents();
+        ob_clean();
+        output('Hello world', 'warn');
+        $outputWarnString = ob_get_contents();
+        ob_clean();
+        output('Hello world', 'err');
+        $outputErrString = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals(
+            "\033[0;32mHello world\033[0m",
+            $outputInfoString
+        );
+        $this->assertEquals(
+            "\033[1;31mHello world\033[0m",
+            $outputWarnString
+        );
+        $this->assertEquals(
+            "\033[1;37m\033[41mHello world\033[0m",
+            $outputErrString
+        );
+    }
+
+    /**
+     * @covers output()
+     * @group console
+     */
+    public function test_output_return_only()
+    {
+        $this->assertEquals(
+            "\033[0;32mHello world\033[0m",
+            output('Hello world', 'info', 0, true)
+        );
+        $this->assertEquals(
+            "\033[1;31mHello world\033[0m",
+            output('Hello world', 'warn', 0, true)
+        );
+        $this->assertEquals(
+            "\033[1;37m\033[41mHello world\033[0m",
+            output('Hello world', 'err', 0, true)
+        );
     }
 
     /**
@@ -51,7 +126,49 @@ class ConsoleHelperTest extends TestCase
      */
     public function test_outputln()
     {
-        $this->markTestSkipped();
+        // Since output prints the string directly, it must be captured by the output buffer
+        ob_start();
+        outputln('Hello world', 'info');
+        $outputInfoString = ob_get_contents();
+        ob_clean();
+        outputln('Hello world', 'warn');
+        $outputWarnString = ob_get_contents();
+        ob_clean();
+        outputln('Hello world', 'err');
+        $outputErrString = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals(
+            "\033[0;32mHello world\n\033[0m",
+            $outputInfoString
+        );
+        $this->assertEquals(
+            "\033[1;31mHello world\n\033[0m",
+            $outputWarnString
+        );
+        $this->assertEquals(
+            "\033[1;37m\033[41mHello world\n\033[0m",
+            $outputErrString
+        );
+    }
+
+    /**
+     * @covers output()
+     * @group console
+     */
+    public function test_outputln_return_only()
+    {
+        $this->assertEquals(
+            "\033[0;32mHello world\n\033[0m",
+            outputln('Hello world', 'info', 0, true)
+        );
+        $this->assertEquals(
+            "\033[1;31mHello world\n\033[0m",
+            outputln('Hello world', 'warn', 0, true)
+        );
+        $this->assertEquals(
+            "\033[1;37m\033[41mHello world\n\033[0m",
+            outputln('Hello world', 'err', 0, true)
+        );
     }
 
     /**
@@ -60,6 +177,28 @@ class ConsoleHelperTest extends TestCase
      */
     public function test_od()
     {
-        $this->markTestSkipped();
+        // Since output prints the string directly, it must be captured by the output buffer
+        ob_start();
+        output('Hello world', 'info');
+        $outputInfoString = ob_get_contents();
+        ob_clean();
+        output('Hello world', 'warn');
+        $outputWarnString = ob_get_contents();
+        ob_clean();
+        output('Hello world', 'err');
+        $outputErrString = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals(
+            "\033[0;32mHello world\033[0m",
+            $outputInfoString
+        );
+        $this->assertEquals(
+            "\033[1;31mHello world\033[0m",
+            $outputWarnString
+        );
+        $this->assertEquals(
+            "\033[1;37m\033[41mHello world\033[0m",
+            $outputErrString
+        );
     }
 }
