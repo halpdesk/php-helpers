28965525630bc02e7ebc226e975f2b741276fe8a
Daniel Leppänen <daniel.leppanen@gmail.com>
2018-12-05 01:32:03 +0100

    Updated ArrayKeyHelpersTest with functions for all array key functions

--- tests/ArrayKeyHelperTest.php
+++ tests/ArrayKeyHelperTest.php
@@ -9,13 +9,49 @@ use PHPUnit\Framework\TestCase;
  */
 class ArrayKeyHelperTest extends TestCase
 {
+    protected $testArray = [
+        'UPPERCASE'     => true,
+        'lowercase'     => true,
+        'iNVERTER'      => true,
+        'Pascal'        => true,
+        'PascalCat'     => true,
+        'kebab-case'    => true,
+        'snake_case'    => true,
+        'DEEP_ARRAY' => [
+            'UPPERCASE'     => true,
+            'lowercase'     => true,
+            'iNVERTER'      => true,
+            'Pascal'        => true,
+            'PascalCat'     => true,
+            'kebab-case'    => true,
+            'snake_case'    => true,
+        ]
+    ];
+
     /**
      * @covers array_keys_to_camel_case()
      * @group array_keys
      */
     public function test_array_keys_to_camel_case()
     {
-        $this->markTestSkipped();
+        $this->assertEquals([
+            'uPPERCASE' => true,
+            'lowercase' => true,
+            'iNVERTER'  => true,
+            'pascal'    => true,
+            'pascalCat' => true,
+            'kebabCase' => true,
+            'snakeCase' => true,
+            'dEEPARRAY' => [
+                'uPPERCASE' => true,
+                'lowercase' => true,
+                'iNVERTER'  => true,
+                'pascal'    => true,
+                'pascalCat' => true,
+                'kebabCase' => true,
+                'snakeCase' => true,
+            ]
+        ], array_keys_to_camel_case($this->testArray));
     }
 
     /**
@@ -24,7 +60,24 @@ class ArrayKeyHelperTest extends TestCase
      */
     public function test_array_keys_to_snake_case()
     {
-        $this->markTestSkipped();
+        $this->assertEquals([
+            'u_p_p_e_r_c_a_s_e' => true,
+            'lowercase'         => true,
+            'i_n_v_e_r_t_e_r'   => true,
+            'pascal'            => true,
+            'pascal_cat'        => true,
+            'kebab_case'        => true,
+            'snake_case'        => true,
+            'd_e_e_p__a_r_r_a_y' => [
+                'u_p_p_e_r_c_a_s_e'     => true,
+                'lowercase'             => true,
+                'i_n_v_e_r_t_e_r'       => true,
+                'pascal'                => true,
+                'pascal_cat'            => true,
+                'kebab_case'            => true,
+                'snake_case'            => true,
+            ]
+        ], array_keys_to_snake_case($this->testArray));
     }
 
     /**
@@ -33,7 +86,24 @@ class ArrayKeyHelperTest extends TestCase
      */
     public function test_array_keys_to_ucfirst()
     {
-        $this->markTestSkipped();
+        $this->assertEquals([
+            'UPPERCASE'     => true,
+            'Lowercase'     => true,
+            'INVERTER'      => true,
+            'Pascal'        => true,
+            'PascalCat'     => true,
+            'Kebab-case'    => true,
+            'Snake_case'    => true,
+            'DEEP_ARRAY' => [
+                'UPPERCASE'     => true,
+                'Lowercase'     => true,
+                'INVERTER'      => true,
+                'Pascal'        => true,
+                'PascalCat'     => true,
+                'Kebab-case'    => true,
+                'Snake_case'    => true,
+            ]
+        ], array_keys_to_ucfirst($this->testArray));
     }
 
     /**
@@ -42,6 +112,23 @@ class ArrayKeyHelperTest extends TestCase
      */
     public function test_array_keys_to_lcfirst()
     {
-        $this->markTestSkipped();
+        $this->assertEquals([
+            'uPPERCASE'     => true,
+            'lowercase'     => true,
+            'iNVERTER'      => true,
+            'pascal'        => true,
+            'pascalCat'     => true,
+            'kebab-case'    => true,
+            'snake_case'    => true,
+            'dEEP_ARRAY' => [
+                'uPPERCASE'     => true,
+                'lowercase'     => true,
+                'iNVERTER'      => true,
+                'pascal'        => true,
+                'pascalCat'     => true,
+                'kebab-case'    => true,
+                'snake_case'    => true,
+            ]
+        ], array_keys_to_lcfirst($this->testArray));
     }
 }
